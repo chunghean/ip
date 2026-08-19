6e90e967776b30b34eee8a183a4f693e32e9cea2
Chung Hean <Chean1503@gmail.com>
2026-08-20 00:14:58 +0800

    Add task storage and list display
    Store entered tasks sequentially, display them with numbering when
    the list command is used, and prevent adding more than 100 tasks.

--- src/main/java/BingusDingus.java
+++ src/main/java/BingusDingus.java
@@ -14,16 +14,38 @@ public class BingusDingus {
         System.out.println("How can I help ya?");
         System.out.println("-".repeat(30));
 
+        // Initialize array to store user text
+        String[] textList = new String[100];
+        int textCount = 0;
+
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
-            System.out.println(command);
-            System.out.println("-".repeat(30));
 
             if (command.equals("bye")) {
                 System.out.println("Bye bye!");
                 System.out.println("-".repeat(30));
                 break;
+            }
+
+            else if (command.equals("list")) {
+                for (int i = 0; i < textCount; i++) {
+                    System.out.println((i + 1) + ". " + textList[i]);
+                }
+                System.out.println("-".repeat(30));
+            }
+
+            else {
+                if (textCount >= textList.length) {
+                    System.out.println("Sorry, you can only store 100 tasks.");
+                    System.out.println("-".repeat(30));
+                    continue;
+                }
+
+                textList[textCount] = command;
+                textCount++;
+                System.out.println("added: " + command);
+                System.out.println("-".repeat(30));
             }
         }
     }
