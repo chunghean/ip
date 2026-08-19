485839dc732ace27e261b06d8267c8626a377fba
Chung Hean <Chean1503@gmail.com>
2026-08-19 23:14:40 +0800

    Rename chatbot to Bingus Dingus

---
+++ src/main/java/BingusDingus.java
@@ -0,0 +1,11 @@
+public class BingusDingus {
+    public static void main(String[] args) {
+        String banner = "        .-\"\"\"\"-.\n"
+                + "       /  o  o  \\\n"
+                + "      |    ∆     |     BINGUS\n"
+                + "      |  \\___/   |     DINGUS\n"
+                + "       \\        /\n"
+                + "        '-.__.-'";
+        System.out.println(banner);
+    }
+}
