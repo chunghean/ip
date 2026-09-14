95316547edac7be97b0fa9e13b9f522dd6155389
Chung Hean <Chean1503@gmail.com>
2026-09-15 03:14:47 +0800

    Set GUI window title
    Set the JavaFX application window title for clearer
    application identification.

--- src/main/java/bingusdingus/gui/BingusDingusGui.java
+++ src/main/java/bingusdingus/gui/BingusDingusGui.java
@@ -23,6 +23,7 @@ public class BingusDingusGui extends Application {
             BorderPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Bingus Dingus");
 
             stage.setMinHeight(620);
             stage.setMinWidth(417);
